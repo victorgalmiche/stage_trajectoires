a2a8d463ac76905fd38fb67efcdafb13266cbf09
victorgalmiche <victorgalmiche@gmail.com>
2026-05-29 10:08:16 +0000

    started analysis of cereq data

---
+++ src/data_analysis.R
@@ -0,0 +1,36 @@
+library(haven)
+library(aws.s3)
+library(dplyr)
+
+BUCKET <- 'victorgalmiche'
+FOLDER <- 'stage-trajectoires/lil-1439/lil-1439.dta/Stata'
+FILE_INDIV <- paste(FOLDER, 'g107individusvf.dta', sep='/')
+FILE_EMPLOI <- paste(FOLDER, 'g107seqentrvf.dta', sep='/')
+FILE_NON_EMPLOI <- paste(FOLDER, 'g107nonemplvf.dta', sep='/')
+
+
+individus <- aws.s3::s3read_using(
+  FUN = haven::read_dta,
+  object = FILE_INDIV,
+  bucket = BUCKET,
+  opts = list("region"="")
+)
+
+emplois <- aws.s3::s3read_using(
+  FUN = haven::read_dta,
+  object = FILE_EMPLOI,
+  bucket = BUCKET,
+  opts = list("region"="")
+)
+
+# Assigning the id (ie the row number in individus)
+emplois$id <- match(emplois$IDENT, individus$IDENT) 
+# Extracting the states
+emplois$state <- as.integer(
+  substr(iconv(emplois$CONTRAT_EMB, to='UTF-8', sub=''), 1, 2))
+# And the sojourn time
+emplois$time <- emplois$DUREE
+
+# Now, creating the dataframe used for analysis
+dataframe <- emplois[, c('id', 'state', 'time')]
+
