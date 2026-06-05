eb30546abfdc3ec38a856c5bd9b88438d51bad8c
victorgalmiche <victorgalmiche@gmail.com>
2026-06-05 08:35:28 +0000

    completed RF implementation and tested on mvad dataset

--- src/random_forest.R
+++ src/random_forest.R
@@ -9,7 +9,7 @@ random_forest <- function(dataframe, covariates, pval_algo,
                           max_features='sqrt', max_samples) {
   cl <- makeCluster(detectCores() - 1)
   registerDoParallel(cl)
-  on.exit(stopCluster(cl))
+  on.exit(stopCluster(cl), add=TRUE)
   
   clusterEvalQ(cl, {
     source('src/semi_markov/synthesis_data_generation.R')
@@ -21,17 +21,21 @@ random_forest <- function(dataframe, covariates, pval_algo,
   forest <- foreach(
     i = 1:n_trees,
     .combine = c,
+    .multicombine = TRUE,
+    .maxcombine = n_trees,
+    .errorhandling = "remove",
     .export = c(
       "dataframe", "covariates", "pval_algo", "ids", "max_samples",
-      "min_obs", "min_leaf", "alpha", "max_depth", "max_features",
-      "build_tree", "find_best_split", "best_split_categorical",
-      "best_split_numeric", "generate_bipartitions")
+      "min_obs", "min_leaf", "alpha", "max_depth", "max_features")
   ) %dopar% { 
     boot_ids <- sample(ids, size = max_samples, replace = TRUE)
-    bootstrap_sample <- subset(dataframe, id %in% boot_ids) 
+    # Need to do this to keep multiple-selected ids
+    idx <- unlist(lapply(boot_ids, function(id) which(dataframe$id == id)))
+    bootstrap_sample <- dataframe[idx, ] 
     
-    build_tree(bootstrap_sample, covariates, pval_algo,
-               max_features, min_obs, min_leaf, alpha, max_depth)
+    # Wrap into a list for the aggregation
+    list(build_tree(bootstrap_sample, covariates, pval_algo,
+               max_features, min_obs, min_leaf, alpha, max_depth))
   }
   
   return(forest)
