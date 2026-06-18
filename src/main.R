dc09e19db9d4a92997c01ff6bacbf96e0a3ca07a
victorgalmiche <victorgalmiche@gmail.com>
2026-06-18 08:30:22 +0000

    cleaned

--- src/main.R
+++ src/main.R
@@ -59,7 +59,7 @@ ranking_MDI <- MDI_all(rf, covariates)
 
 
 barplot(ranking_MDI, 
-        main = "Permutation test and Gamma Law",
+        main = "Chi^2 test and Gamma Law",
         ylab = "MDI", 
         ylim = c(0,1),
         col = "blue", 
