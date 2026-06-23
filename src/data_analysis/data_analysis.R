35b10ea4e41c144fab48e08ef78dfa81d19fce49
victorgalmiche <victorgalmiche@gmail.com>
2026-06-23 10:20:45 +0000

    description of the data

--- src/data_analysis/data_analysis.R
+++ src/data_analysis/data_analysis.R
@@ -60,6 +60,65 @@ dataframe <- aggregate(time ~ group + id + state, data = cbind(df_merged, group)
 dataframe <- dataframe[order(dataframe$group), c("id", "state", "time")]
 
 
+# Descriptive statistics
+state_labels <- c('Employment', 'Interim', 'Job Search', 
+                  'School', 'Training', 'Others')
+df_sum <- dataframe %>%
+  group_by(state) %>%
+  summarise(total_time = sum(time, na.rm = TRUE))
+
+barplot(df_sum$total_time,
+        # log = 'y',
+        names.arg = state_labels,
+        xlab = "State",
+        ylab = "Time in months",
+        # ylim = c(1e+03, 1e+06),
+        main = "Cumulative time spent in each state"
+)
+
+barplot(table(dataframe$state), 
+     names.arg = state_labels, 
+     xlab = 'State', 
+     ylab = 'Number of observations', 
+     ylim = c(0, 2e+04),
+     main = 'Number of observations for each state')
+
+hist(dataframe$time,
+     xlab = 'Time in months', 
+     ylab = 'Number of observations', 
+     main = 'Histogram of sojourn times')
+
+
+employment <- dataframe[dataframe$state==1, ]
+library(MASS)
+fit_exp <- fitdistr(employment$time, "exponential")
+fit_gamma <- fitdistr(employment$time, "gamma")
+fit_weibull <- fitdistr(employment$time, "weibull")
+
+x_vals <- seq(0, max(employment$time), length.out = 100)
+hist(employment$time, 
+     breaks = x_vals,
+     freq = FALSE,
+     xlab = 'Time in months', 
+     ylab = 'Density', 
+     main = 'Histogram of sojourn times in employment')
+lines(x_vals, 
+      dgamma(x_vals, shape=fit_gamma$estimate[['shape']], rate=fit_gamma$estimate[['rate']]), 
+      col = "red")
+lines(x_vals, 
+      dweibull(x_vals, shape=fit_weibull$estimate[['shape']], scale=fit_weibull$estimate[['scale']]),
+      col = "green")
+lines(x_vals, 
+      dexp(x_vals, rate=fit_exp$estimate),
+      col="blue")
+legend("top",
+       legend = c("Gamma", "Weibull", "Exponential", paste0("n = ", nrow(employment))),
+       col    = c("red", "green", "blue", NA),
+       lty    = c(1, 1, 1, NA))
+
+
+
+
 # To obtain the trajectories
 trajectories <- dataframe %>%
   group_by(id) %>%
