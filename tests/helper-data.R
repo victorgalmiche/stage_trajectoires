e9001db9e92e5d05e9f497a7052f749fbd522dff
victorgalmiche <victorgalmiche@gmail.com>
2026-07-06 13:23:19 +0000

    fix

--- tests/helper-data.R
+++ tests/helper-data.R
@@ -1,11 +1,22 @@
 source_from_root <- function(path) {
-  source(file.path(testthat::test_path("..", ".."), path))
+  root <- normalizePath(file.path(testthat::test_path(), "..", ".."), winslash = "/")
+  source(file.path(root, path), local = FALSE)
 }
 
+# --- sources du projet ---
 source_from_root("src/semi_markov/mle_estimation.R")
 source_from_root("src/semi_markov/synthesis_data_generation.R")
-source_from_root("src/two_samples_test.R")
-source_from_root("src/random_forest/tree_construction.R")
+# source_from_root("src/two_samples_test.R")        
+# source_from_root("src/random_forest/tree_construction.R")
+
+# --- fixtures ---
+make_simple_df <- function() {
+  data.frame(
+    id    = c(1, 1, 2, 2),
+    state = c(1, 2, 1, 3),
+    time  = c(5, 3, 2, 4)
+  )
+}
 
 make_toy_trajectories <- function(n = 30, D = 4, M = 5, seed = 1) {
   set.seed(seed)
--- tests/testthat/test-mle.R
+++ tests/testthat/test-mle.R
@@ -1,12 +1,3 @@
-# Function to make a simple dataframe w/ 2 individuals and 3 different states
-make_simple_df <- function() {
-  data.frame(
-    id    = c(1, 1, 2, 2),
-    state = c(1, 2, 1, 3),
-    time  = c(5, 3, 2, 4)
-  )
-}
-
 test_that("alpha_hat sums to 1", {
   dataframe <- make_simple_df()
   alpha_hat <- mle_fit(dataframe, D=3)$estimator$alpha
