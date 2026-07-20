01bed9a11cf11b93ff7fe20a16f87129c73d3543
victorgalmiche <victorgalmiche@gmail.com>
2026-07-20 12:24:19 +0000

    split the generation of synthesis parameters

--- src/semi_markov/synthesis_data_generation.R
+++ src/semi_markov/synthesis_data_generation.R
@@ -2,34 +2,48 @@
 ### Generate parameters defining an SMP
 # D is the number of states
 # law_sojourn can be Gamma, Weibull or Exponential
-generate_theta <- function(D, law_sojourn='gamma'){
-  # Initial probabilities 
+generate_alpha <- function(D){
   alpha <- runif(D)
-  alpha <- alpha/sum(alpha) # sum = 1
-  
-  # Transition matrix
+  alpha/sum(alpha) # sum = 1
+}
+
+generate_P <- function(D){
   P <- matrix(runif(D^2), nrow=D)
   diag(P) <- 0 # diagonal coefficients are 0
-  P <- P/rowSums(P) # each row sum to one 
+  P/rowSums(P) # each row sum to one 
+}
+
+generate_omega <- function(D, law_sojourn) {
+  switch(law_sojourn,
+         gamma = {
+           a <- rexp(D, rate = 0.5) # shape
+           lambda <- rexp(D, rate = 3) # rate=1/scale
+           cbind(shape = a, rate = lambda)
+         },
+         weibull = {
+           eta <- rexp(D, rate = 0.8) # shape
+           beta <- rexp(D, rate = 0.15) # scale
+           cbind(shape = eta, scale = beta)
+         },
+         exponential = {
+           lambda <- rexp(D, rate = 1) # rate
+           cbind(rate = lambda)
+         },
+         stop(paste("Unknown sojourn law:", law_sojourn))  # input validation
+         )
+}
+
+
+generate_theta <- function(D, law_sojourn='gamma'){
+  # Initial probabilities 
+  alpha <- generate_alpha(D)
+  
+  # Transition matrix
+  P <- generate_P(D)
   
   # Sojourn times
-  omega <- switch(law_sojourn,
-                  gamma = {
-                    a <- rexp(D, rate = 0.5) # shape
-                    lambda <- rexp(D, rate = 3) # rate=1/scale
-                    cbind(shape = a, rate = lambda)
-                  },
-                  weibull = {
-                    eta <- rexp(D, rate = 0.8) # shape
-                    beta <- rexp(D, rate = 0.15) # scale
-                    cbind(shape = eta, scale = beta)
-                  },
-                  exponential = {
-                    lambda <- rexp(D, rate = 1) # rate
-                    cbind(rate = lambda)
-                  },
-                  stop(paste("Unknown sojourn law:", law_sojourn))  # input validation
-  )
+  omega <- generate_omega(D, law_sojourn)
+
   return (list(alpha=alpha, P=P, omega=omega))
 }
 
