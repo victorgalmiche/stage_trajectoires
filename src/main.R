6ebf837b97e0b674eb5303cbe8f3d4beb0c363e5
victorgalmiche <victorgalmiche@gmail.com>
2026-07-31 07:57:07 +0000

    variable importance measures

--- src/main.R
+++ src/main.R
@@ -16,8 +16,7 @@ source('src/visualization.R')
 
 law_sojourn <- 'exponential'
 
-df_PHD1 <- subset(dataframe, id %in% which(covariates$PHD==1))
-
+# Tree construction
 system.time({
   min_leaf <- as.integer(floor(length(unique(dataframe$id))/20)) # 5% of the total nb of ind
   tree <- build_tree(dataframe, covariates, weights, 
@@ -25,30 +24,36 @@ system.time({
                      min_leaf = min_leaf, alpha = 0.05, max_depth = 5)
 })
 
+# RF construction
 system.time({
   forest <- random_forest(dataframe, covariates, weights, 
                           D, law_sojourn, likelihood_ratio_test, 
                           min_leaf = min_leaf, alpha = 0.05)
 })
 
-system.time({
-  ranking_MDA <- MDA_all(forest, df_PHD1, covariates, 
-                         D, weights, law_sojourn)
-})
-
+# MDI
 system.time({
   ranking_MDI <- MDI_all(forest, covariates)
 })
 
+barplot(ranking_MDI, 
+        main = "Mean Decrease Impurity for each covariate",
+        ylab = "MDI", 
+        ylim = c(0,1),
+        col = "blue", 
+        las = 2)
+
+
+# MDA
+system.time({
+  ranking_MDA <- MDA_all(forest, dataframe, covariates, 
+                         D, weights, law_sojourn)
+})
+
 barplot(ranking_MDA, 
-        main = "Permutation test and Exponential Law - PHD=1",
+        main = "Mean Decrease Acurracy for each covariate",
         ylab = "MDA",
         col = "blue", 
         las = 2)
 
-barplot(ranking_MDI, 
-        main = "Permutation test and Exponential Law - PHD=1",
-        ylab = "MDI", 
-        # ylim = c(0,1),
-        col = "blue", 
-        las = 2)
+
