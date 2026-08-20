835a5c7433ed095967ada3decfb3db92fa706810
victorgalmiche <victorgalmiche@gmail.com>
2026-08-20 12:56:09 +0000

    simulation study for execution time

--- simulations/time_test.R
+++ simulations/time_test.R
@@ -2,9 +2,9 @@ library(doParallel)
 library(foreach)
 
 run_simulation <- function(cl, D, n, law_sojourn, R,
-                           M = 5, nb_datasets=500){
+                           T_max = 5, nb_datasets=500){
   
-  clusterExport(cl, varlist = c("D", "n", "M", "law_sojourn", "R"),
+  clusterExport(cl, varlist = c("D", "n", "T_max", "law_sojourn", "R"),
                 envir = environment())
   
   results <- foreach(
@@ -13,7 +13,7 @@ run_simulation <- function(cl, D, n, law_sojourn, R,
   ) %dopar% {  
     
     theta <- generate_theta(D, law_sojourn)
-    df <- generate_dataset_H0(theta, law_sojourn, n, n, M)
+    df <- generate_dataset_H0(theta, law_sojourn, n, n, T_max)
     
     df1 <- subset(df, id<=n)
     df2 <- subset(df, id>n)
@@ -24,7 +24,7 @@ run_simulation <- function(cl, D, n, law_sojourn, R,
     
     t_boot <- system.time(
       p_boot <- parametric_bootstrap(df1, df2, D, law_sojourn = law_sojourn, 
-                                     M = M, R = R)
+                                     T_max = T_max, R = R)
     )
     
     t_perm <- system.time(
@@ -38,27 +38,12 @@ run_simulation <- function(cl, D, n, law_sojourn, R,
   
   list(
     D = D, n = n, law_sojourn = law_sojourn, R = R,
-    M = M, nb_datasets = nb_datasets,
+    T_max = T_max, nb_datasets = nb_datasets,
     t_asymp = results[, "t_asymp"],
     t_boot = results[, "t_boot"],
     t_perm = results[, "t_perm"]
   )
 }
-
-plot_times <- function(sim_result) {
-  df_time <- data.frame(
-    time   = c(sim_result$t_asymp, sim_result$t_boot, sim_result$t_perm),
-    algo = rep(c("Chi^2", "Bootstrap", "Permutation"),
-                  each = sim_result$nb_datasets)
-  )
-  
-  boxplot(time ~ algo, data = df_time,
-          log = 'y',
-          main = "Time Executions Comparison",
-          xlab = "", ylab = "Time (seconds, in log-scale)",
-          col = c("#66c2a5", "#fc8d62", "#8da0cb"))
-}
-
 
 n_cores <- detectCores() - 1
 cl <- makeCluster(n_cores)
@@ -69,9 +54,52 @@ clusterEvalQ(cl, {
   source('src/two_samples_test.R')
 })
 
-res <- run_simulation(cl, D = 10, n = 200, 
-                      law_sojourn = 'exponential', R = 100)
-plot_times(res)
+res <- run_simulation(cl, D = 10, n = 30, 
+                      law_sojourn = 'exponential', R = 500, 
+                      nb_datasets = 50)
+
+# Varying n, number of trajectories
+n_vals <- c(30, 60, 100, 200)
+asymp_D4 <- c(0.03694, 0.05298, 0.05596, 0.102)
+asymp_D10 <- c(0.049, 0.0595, 0.0773, 0.14406)
+boot_D4 <- c(45.26, 74.92, 120.35, 216.74)
+boot_D10 <- c(52.17, 90.37, 154.58, 308.10)
+perm_D4 <- c(0.99794, 1.34, 1.82, 2.93)
+perm_D10 <- c(1.40, 1.589, 2.2382, 3.53)
+
+plot(n_vals, asymp_D4, col='red', pch=1, type='b',
+     xlab='Number of trajectories in each group (n)', 
+     ylab='Time in seconds (log-scale)', log='y', ylim=c(1e-2, 5e2),
+     main='Execution Time Comparison')
+points(n_vals, asymp_D10, col='red', pch=2, type='b')
+points(n_vals, boot_D4, col='blue', pch=1, type='b')
+points(n_vals, boot_D10, col='blue', pch=2, type='b')
+points(n_vals, perm_D4, col='green', pch=1, type='b')
+points(n_vals, perm_D10, col='green', pch=2, type='b')
+
+
+legend("center", 
+       legend = c("Chi^2", "Permutation", "Parametric Bootstrap", "D=4", "D=10"),
+       col = c("red", "green", "blue", 'grey', 'grey'),
+       pch = c(NA, NA, NA, 1, 2), 
+       lty = c(1, 1, 1, NA, NA))
+
+
+
+# Varying R number of permutation/bootstrap sample
+R_vals <- c(100, 500, 1000)
+boot_D4 <- c(45.26, 217.64, 438.52)
+boot_D10 <- c(52.17, 247.09, 494.27)
+perm_D4 <- c(0.99794, 4.96, 10.45)
+perm_D10 <- c(1.40, 6.75, 13.56)
+
+plot(R_vals, boot_D4, col='blue', pch=1, type='b',
+     xlab='R', 
+     ylab='Time in seconds (log-scale)', log='y', ylim=c(1, 5e2),
+     main='Execution Time Comparison')
+points(R_vals, boot_D10, col='blue', pch=2, type='b')
+points(R_vals, perm_D4, col='green', pch=1, type='b')
+points(R_vals, perm_D10, col='green', pch=2, type='b')
 
 stopCluster(cl)
 
