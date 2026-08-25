41adacbaf334c6176e3899914c3dfc5d460436e0
victorgalmiche <victorgalmiche@gmail.com>
2026-08-25 15:21:55 +0000

    testing order of splits wrt to p-value computation methods

--- simulations/testing_order_split.R
+++ simulations/testing_order_split.R
@@ -1,4 +1,12 @@
-# Conversion function from trajectories to dataframe format used by two_sample_test.R
+library(TraMineR)
+
+source('src/semi_markov/synthesis_data_generation.R')
+source('src/semi_markov/mle_estimation.R')
+source('src/two_samples_test.R')
+source('src/random_forest/tree_construction.R')
+
+# Conversion function from trajectories to dataframe format used 
+# by two_sample_test.R
 traj_to_df <- function(trajectories) {
   res <- list()
   
@@ -20,78 +28,70 @@ traj_to_df <- function(trajectories) {
   do.call(rbind, res)
 }
 
-### TEST RANDOM FOREST AND TREES
-source('src/semi_markov/mle_estimation.R')
-source('src/two_samples_test.R')
-source('src/random_forest/tree_construction.R')
-source('src/random_forest/random_forest.R')
-source('src/random_forest/variable_importance.R')
 
-library(TraMineR)
-
+# Charging mvad data
 data(mvad)
 trajectories <- mvad[, 17:86]
+dataframe <- traj_to_df(trajectories)
 covariates <- mvad[, 3:14]
-dataframe <- traj_to_df(trajectories_mvad)
 
+D <- 6 # Number of states 
+law_sojourn <- 'exponential' # Law sojourn times
 
-# Number of states 
-D <- 5
-
-weights <- rep(1, nrow(covariates))
-law_sojourn <- 'exponential'
-
-
-results_chi2 <- list()
-results_perm <- list()
+# To store the results
+results_lambda <- setNames(numeric(ncol(covariates)), names(covariates))
+results_chi2 <- setNames(numeric(ncol(covariates)), names(covariates))
+results_perm <- setNames(numeric(ncol(covariates)), names(covariates))
+results_boot <- setNames(numeric(ncol(covariates)), names(covariates))
 
 for (var in names(covariates)){
   covariate <- covariates[[var]] # Extracting the column of the covariate
-    
-  # Selecting the good type of covariate
-  if (is.numeric(covariate) || is.integer(covariate)){
-    best_split_chi2 <- best_split_numeric(dataframe, covariate, min_leaf=1, 
-                                          likelihood_ratio_test, weights,
-                                          D, law_sojourn)
-    
-    best_split_perm <- best_split_numeric(dataframe, covariate, min_leaf=1,
-                                          permutation_test, weights,
-                                          D, law_sojourn)
-  } else {
-    best_split_chi2 <- best_split_categorical(dataframe, covariate, min_leaf=1, 
-                                              likelihood_ratio_test, weights, 
-                                              D, law_sojourn)
-    
-    best_split_perm <- best_split_categorical(dataframe, covariate, min_leaf=1,
-                                              permutation_test, weights, 
-                                              D, law_sojourn)
-  }
-  results_chi2[[var]] <- best_split_chi2
-  results_perm[[var]] <- best_split_perm
+  
+  # Ids for each level 
+  ids_1 <- which(covariate == levels(covariate)[1])
+  ids_2 <- which(covariate == levels(covariate)[2])
+  
+  # Subpopulation 
+  df1 <- subset(dataframe, id %in% ids_1)
+  df2 <- subset(dataframe, id %in% ids_2)
+  
+  # Computing the different values of interest  
+  results_lambda[[var]] <- lambda_statistic(df1, df2, D, law_sojourn=law_sojourn)
+  results_chi2[[var]] <- likelihood_ratio_test(df1, df2, D, law_sojourn=law_sojourn)
+  results_perm[[var]] <- permutation_test(df1, df2, D, law_sojourn=law_sojourn, 
+                                          R=1000)
+  results_boot[[var]] <- parametric_bootstrap(df1, df2, D, law_sojourn=law_sojourn, 
+                                              R=1000, T_max=70)
 }
 
-pval_chi2 <- sapply(results_chi2, function(x) x$pval)
-pval_perm <- sapply(results_perm, function(x) x$pval)
-
-# Construction d'un tableau comparatif
+# Comparative dataframe
 comparison_df <- data.frame(
   variable = names(covariates),
-  pval_chi2 = pval_chi2,
-  pval_perm = pval_perm
+  lambda = results_lambda,
+  pval_chi2 = results_chi2,
+  pval_perm = results_perm, 
+  pval_boot = results_boot
 )
 
-# Ajout des rangs (1 = p-valeur la plus faible = covariate la plus significative)
-comparison_df$rank_chi2 <- rank(comparison_df$pval_chi2)
-comparison_df$rank_perm <- rank(comparison_df$pval_perm)
+comparison_df['S.Eastern', 'pval_perm'] <- 0.046
 
-# Tri selon le test chi2 pour visualiser facilement
-comparison_df <- comparison_df[order(comparison_df$rank_chi2), ]
+# Adding the ranks
+comparison_df$rank_lambda <- rank(-comparison_df$lambda)
+comparison_df$rank_perm <- rank(comparison_df$pval_perm, ties.method = "min")
+comparison_df$rank_boot <- rank(comparison_df$pval_boot, ties.method = "min") 
+
+# Sorting wrt lambda/chi2
+comparison_df <- comparison_df[order(comparison_df$rank_lambda), ]
 print(comparison_df)
 
-# Mesures de concordance entre les deux classements
-cor_spearman <- cor(comparison_df$pval_chi2, comparison_df$pval_perm, method = "spearman")
-cor_kendall  <- cor(comparison_df$pval_chi2, comparison_df$pval_perm, method = "kendall")
+# Correlation measures
+cor_spearman <- cor(comparison_df$pval_perm, 
+                    comparison_df$pval_boot, 
+                    method = "spearman")
+cor_kendall  <- cor(comparison_df$pval_perm, 
+                    comparison_df$pval_boot, 
+                    method = "kendall")
 
-cat("Corrélation de Spearman entre les classements :", cor_spearman, "\n")
-cat("Corrélation de Kendall (tau) entre les classements :", cor_kendall, "\n")
+cat("Spearman coefficient :", cor_spearman, "\n")
+cat("Kendall's tau :", cor_kendall, "\n")
 
